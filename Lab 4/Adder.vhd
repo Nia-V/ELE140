425d20060bccdc18d57c62ebaf067d5beb77abb5
Nia-V <116529978+Nia-V@users.noreply.github.com>
2025-11-12 13:24:35 -0500

    LAb 4 first commit adder

---
+++ Lab 4/Adder.vhd
@@ -0,0 +1,22 @@
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity Adder is
+    port(
+        A   : in  std_logic;
+        B   : in  std_logic;
+        Cin : in  std_logic := '0';  -- input carry / previous stage
+        Cout : out std_logic;   -- Carry output next stage
+		  S    : out std_logic
+    );
+end Adder;
+
+architecture add_arch of Adder is
+begin
+process (A,B,Cin)
+begin
+	S <= A xor B xor Cin ;
+   Cout <= (((A xor B) and Cin) or (A and B));
+end process;
+end add_arch;
